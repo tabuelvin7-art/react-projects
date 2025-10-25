54350a1a6cdb2d8c155debe69c744f0d5cbab735
tabuelvin7-art <tabuelvin7@gmail.com>
2025-10-25 15:54:11 +0300

    Made the search functionality work

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -1,10 +1,24 @@
-import {Link} from 'react-router';  
+import {Link, useNavigate, useSearchParams} from 'react-router';  
 import './header.css'; 
+import { useState } from 'react';
 export function Header({cart}) {
+    const navigate = useNavigate();
+    const [searchParams] = useSearchParams();
+    
+
+    const searchText = searchParams.get('search');
+    const [search, setSearch] = useState(searchText || '');
+    
+    const updateSearchInput = (event) => {
+        setSearch(event.target.value);
+    }
+    const searchProducts = () => {
+        navigate(`/?search=${search}`);
+    }
     let totalQuantity = 0;
 
     cart.forEach((cartItem) => {
-        totalQuantity += cartItem.quantity;
+         totalQuantity += cartItem.quantity;
     });
 
     return (
@@ -20,9 +34,11 @@ export function Header({cart}) {
         </div>
 
         <div className="middle-section">
-            <input className="search-bar" type="text" placeholder="Search" />
+            <input className="search-bar" type="text" placeholder="Search"
+            value={search} onChange={updateSearchInput} />
 
-            <button className="search-button">
+            <button className="search-button"
+            onClick={searchProducts}>
             <img className="search-icon" src="images/icons/search-icon.png" />
             </button>
         </div>
