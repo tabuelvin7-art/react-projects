42ad61c7f2b7641c102080d5d3b8c5df6411674d
tabuelvin7-art <tabuelvin7@gmail.com>
2025-10-28 14:06:49 +0300

    Removed unused imports

--- ecommerce-project/src/pages/home/HomePage.test.jsx
+++ ecommerce-project/src/pages/home/HomePage.test.jsx
@@ -1,7 +1,6 @@
 import { it, expect, describe, vi, beforeEach } from "vitest";
 import { render, screen,within } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
-import userEvent from "@testing-library/user-event";
 import axios from "axios";
 import { HomePage } from "./HomePage";
 
